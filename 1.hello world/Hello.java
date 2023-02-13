db6fd62c39e5e10f58217b119d32d34521d6e1a3
oussamaserhane <48053932+oussamaserhane@users.noreply.github.com>
2023-02-13 12:45:48 +0100

    Add files via upload

---
+++ 1.hello world/Hello.java
@@ -0,0 +1,15 @@
+
+public class Hello{
+	// This is my first java code
+	public static void main(String args[]){	
+		System.out.println("hello Sliman ");
+	}
+	{/*
+		This is a multiline commants part!
+	*/}
+}
+
+
+
+
+
---
+++ 3.2 for loop/forLoop.java
@@ -0,0 +1,13 @@
+public class forLoop {
+
+	public static void main(String[] args) {
+		for(int i = 5; i <= 100 ; i= i+5){
+			System.out.println("i: "+i);
+			}
+			
+	}
+}
+
+
+
+
